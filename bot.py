356f62ffe90cbdf1addc934855b9f8a6bb377514
samkayz <ilemobayosamson@gmail.com>
2021-05-04 16:38:35 +0100

    Changes

--- bot.py
+++ bot.py
@@ -2,23 +2,40 @@ import websocket, json, pprint, talib, numpy
 from decouple import config
 from binance.client import Client
 from binance.enums import *
+import ssl
+import time
+import certifi
+
+
+
+
 
 #### This Is Bot
-SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+SOCKET = "wss://stream.binance.com:9443/ws/irisusdt@kline_1m"
 
-RSI_PERIOD = 14
-RSI_OVERBOUGHT = 70
-RSI_OVERSOLD = 30
-TRADE_SYMBOL = 'ETHUSD'
-TRADE_QUANTITY = 0.05
+RSI_PERIOD = 5
+RSI_OVERBOUGHT = 30
+RSI_OVERSOLD = 100
+TRADE_SYMBOL = 'IRISUSDT'
+TRADE_QUANTITY = 100
 
 closes = []
 in_position = False
 
 API_KEY = config('API_KEY')
 API_SECRET = config('API_SECRET')
-client = Client(API_KEY, API_SECRET, tld='us')
+client = Client(API_KEY, API_SECRET, {"verify": True, "timeout": 60})
 
+fees = client.get_trade_fee(symbol=TRADE_SYMBOL)
+print(fees)
+
+# fee = fees[0]['makerCommission'] 
+
+# print(float(fee))
+
+# S_QUANTITY = TRADE_QUANTITY - float(fee)
+ 
+SELL_QUANTITY = TRADE_QUANTITY - 1
 def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
     try:
         print("sending order")
@@ -30,7 +47,12 @@ def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
 
     return True
 
-    
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+        
 def on_open(ws):
     print('opened connection')
 
@@ -67,7 +89,7 @@ def on_message(ws, message):
                 if in_position:
                     print("Overbought! Sell! Sell! Sell!")
                     # put binance sell logic here
-                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    order_succeeded = order(SIDE_SELL, SELL_QUANTITY, TRADE_SYMBOL)
                     if order_succeeded:
                         in_position = False
                 else:
@@ -83,6 +105,12 @@ def on_message(ws, message):
                     if order_succeeded:
                         in_position = True
 
-                
-ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-ws.run_forever()
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)         
+    ws = websocket.WebSocketApp(SOCKET,
+                              on_open = on_open,
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
